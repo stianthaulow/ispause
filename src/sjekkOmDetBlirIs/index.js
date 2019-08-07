a0fe9a420671a81177a09c05993abc853a8a022a
Stian Thaulow <stian@thaulow.me>
2019-08-07 21:56:59 +0200

    Add packages and env variable

--- src/sjekkOmDetBlirIs/index.js
+++ src/sjekkOmDetBlirIs/index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 
+const yrUrl = process.env.Yr_URL;
+
 module.exports = async function(context, req) {
+  context.log(yrUrl);
+
   const detBlirIs = req.query.is == "ja" || false;
   const fileName = detBlirIs ? "ja.html" : "nei.html";
 
